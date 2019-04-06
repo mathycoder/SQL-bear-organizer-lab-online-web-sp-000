697c71cd9ae4cd8f212c649e2a2aa6a7ed0ff095
Adam S <adam.sarli@gmail.com>
2019-04-06 16:58:05 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES 
+("Mr. Chocolate", 8, "M", "Brown", "gentle",0), 
+("Rowdy", 8, "M", "White", "aggressive",1),
+("Tabitha", 8, "F", "Brown", "gentle",0),
+("Sergeant Brown", 8, "M", "Black", "aggressive",0),
+("Melissa", 8, "F", "Brown", "gentle",1),
+("Grinch", 8, "M", "White", "gentle",0),
+("Wendy", 8, "F", "Brown", "aggressive",0),
+(NULL, 8, "M", "Black", "gentle",1);
